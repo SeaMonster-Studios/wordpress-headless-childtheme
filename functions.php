b3a36ed9cadf06acfc1bbe52cc913d8309544918
Logan Call <logancall.dev@gmail.com>
2018-08-01 16:45:45 -0600

    add tinymce custom buttons

--- functions.php
+++ functions.php
@@ -4,4 +4,5 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 require_once('settings/options-pages.php');
 require_once('settings/support.php');
 require_once('settings/menus.php');
+require_once('settings/tinymce.php');
 require_once('api/index.php');

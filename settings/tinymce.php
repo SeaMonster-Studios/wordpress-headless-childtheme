4d6285cf90f12be5f75ebd7aed5113c3ca7c82e6
Logan Call <logancall.dev@gmail.com>
2018-08-06 11:04:00 -0600

    add heading class options in tinymce editor

--- settings/tinymce.php
+++ settings/tinymce.php
@@ -31,6 +31,42 @@ function my_mce_before_init_insert_formats( $init_array ) {
 			'classes' => 'button button-tertiary',
 			'wrapper' => true,
 		),
+    array(  
+			'title' => 'Heading 1 styles',  
+			'selector' => 'h1,h2,h3,h4,h5,h6',
+			'classes' => 'h1',
+			'wrapper' => false,
+		),
+    array(  
+			'title' => 'Heading 2 styles',  
+			'selector' => 'h1,h2,h3,h4,h5,h6',
+			'classes' => 'h2',
+			'wrapper' => false,
+		),
+    array(  
+			'title' => 'Heading 3 styles',  
+			'selector' => 'h1,h2,h3,h4,h5,h6',
+			'classes' => 'h3',
+			'wrapper' => false,
+		),
+    array(  
+			'title' => 'Heading 4 styles',  
+			'selector' => 'h1,h2,h3,h4,h5,h6',
+			'classes' => 'h4',
+			'wrapper' => false,
+		),
+    array(  
+			'title' => 'Heading 5 styles',  
+			'selector' => 'h1,h2,h3,h4,h5,h6',
+			'classes' => 'h5',
+			'wrapper' => false,
+		),
+    array(  
+			'title' => 'Heading 6 styles',  
+			'selector' => 'h1,h2,h3,h4,h5,h6',
+			'classes' => 'h6',
+			'wrapper' => false,
+		),
 	);  
 	// Insert the array, JSON ENCODED, into 'style_formats'
 	$init_array['style_formats'] = json_encode( $style_formats );  
